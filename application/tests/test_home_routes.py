b7eacc50935b618f5acb4f2e34754d41fd3e4327
Edison <edisonstuart@gmail.com>
2022-06-17 19:45:00 -0400

    Made tests for new route

--- application/tests/test_home_routes.py
+++ application/tests/test_home_routes.py
@@ -17,6 +17,24 @@ def test_about_page():
         response = test_client.get('/about')
         assert response.status_code == 200
 
+def test_form_page():
+    flask_app = create_app()
+    flask_app.testing = True
+
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/form')
+        assert response.status_code == 200
+
+def test_post_form_page():
+    '''Test will eventually have a different page loaded as response...
+        for now post request will return 405 code'''
+    flask_app = create_app()
+    flask_app.testing = True
+
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/form')
+        assert response.status_code == 405
+
 def test_post_home_page():
     flask_app = create_app()
     flask_app.testing = True
