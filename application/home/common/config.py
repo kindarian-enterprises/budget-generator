ff8c0a5c0d4ec65768a348142d3cb454e40f499a
Edison <edisonstuart@gmail.com>
2022-08-16 18:12:21 -0400

    changed how we obtain config file path

--- application/home/common/config.py
+++ application/home/common/config.py
@@ -2,6 +2,11 @@ import configparser
 from pathlib import Path
 import os
 
-CONFIG_FILE = Path(os.path.abspath('application/home/common/appconfig.ini'))
+CONFIG_FILE = os.path.join(
+	os.path.dirname(
+		os.path.realpath(__file__)
+		),
+		'appconfig.ini'
+	)
 APPCONFIG = configparser.ConfigParser()
 APPCONFIG.read(CONFIG_FILE)
