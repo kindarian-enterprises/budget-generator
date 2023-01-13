9f6bc004be45abb706feedceb107fc9d93b15162
Edison <edisonstuart@gmail.com>
2023-01-13 14:37:58 -0500

    Got saved_budgets looking more like the mockup on draw.io

--- application/home/static/saved_budgets.js
+++ application/home/static/saved_budgets.js
@@ -2,7 +2,6 @@
 
 const listObject = document.getElementById("saved_budget_list");
 window.addEventListener("load", function (e) {
-  console.log("Page loaded!");
   fetch("http://127.0.0.1/budget/")
     .then((response) => response.json())
     .then((data) => {
@@ -14,18 +13,38 @@ window.addEventListener("load", function (e) {
     `;
       } else {
         const uListEl = document.createElement("ul");
-        data.forEach((budget) => {
+        data.forEach((budget, i, arr) => {
           const Budget = JSON.parse(budget);
+          const budgetDate = new Date();
+          budgetDate.setTime(Budget.dateCreated.$date);
 
           const listEl = this.document.createElement("li");
 
-          const texNode = document.createTextNode(`
-          Your Goal: $${Budget["goal"]}--Months Until Goal: ${Budget["timeUntilGoal"]}--Spending Money: $${Budget["monthlySpending"]}--Money to be Saved: $${Budget["monthlySaving"]}
-          `);
-          listEl.appendChild(texNode);
+          const textNode1 = document.createTextNode(checkNumLength(Budget));
+          const textNode2 = this.document.createTextNode(
+            `Date Created: ${budgetDate.toLocaleDateString()}`
+          );
+
+          listEl.classList.add(
+            `budget-list-${i === arr.length - 1 ? "element-last" : "element"}`
+          );
+          listEl.appendChild(textNode1);
+          listEl.appendChild(textNode2);
           uListEl.appendChild(listEl);
           listObject.appendChild(uListEl);
         });
       }
     });
 });
+
+function checkNumLength(value) {
+  let numLength = value["goal"].toString().length;
+  // console.log(typeof value["goal"]);
+  if (numLength >= 11) {
+    return `Your Goal: $${value["goal"]}`;
+  } else {
+    const spaceVal = 11 - numLength;
+    // console.log(spaceVal, numLength);
+    return `Your Goal: $${value["goal"]}${"\xa0\xa0".repeat(spaceVal)}`;
+  }
+}
